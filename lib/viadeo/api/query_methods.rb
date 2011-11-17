646ac3f0e547ea517165dc354229a5765c9fe2d6
Gael <gael@gael-ubuntu.(none)>
2011-11-17 12:47:56 +0100

    Now resend the request up to 3 times if failure

--- lib/viadeo/api/query_methods.rb
+++ lib/viadeo/api/query_methods.rb
@@ -14,18 +14,18 @@ module Viadeo
 
 
      	def simple_query(access_token, path, args)
-        	url = "#{DEFAULT_OAUTH_OPTIONS[:api_base]}#{path}?access_token=#{access_token}"
-        	args.each {|key, value| url += "&#{key}=#{value}"}
-			    uri = URI.parse(url)
-			    connection = Net::HTTP.new(uri.host, 443)
-			 	  connection.use_ssl = true
-		 	   	connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
-		 	   	resp = connection.request_get(uri.path + '?' + uri.query)
-		 	   	if resp.code != '200'
-		  	 		raise "web service error"
-			    end
-			   return Mash.from_json resp.body
-	     end
+        url = "#{DEFAULT_OAUTH_OPTIONS[:api_base]}#{path}?access_token=#{access_token}"
+        args.each {|key, value| url += "&#{key}=#{value}"}
+			  uri = URI.parse(url)
+			  (1..3).each do
+					connection = Net::HTTP.new(uri.host, 443)
+				 	connection.use_ssl = true
+			 	  connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
+			 	  resp = connection.request_get(uri.path + '?' + uri.query)
+					return Mash.from_json resp.body if resp.code == '200'
+				end
+				return nil
+			end
 
         def person_path(options)
           path = "/people/"
