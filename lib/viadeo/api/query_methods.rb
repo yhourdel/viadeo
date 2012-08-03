ce76e4dd962bae1f66ec69c1cd1318ad318d475f
gflores <gael.flores@epitech.eu>
2012-08-03 19:56:44 +0300

    Update lib/viadeo/api/query_methods.rb

--- lib/viadeo/api/query_methods.rb
+++ lib/viadeo/api/query_methods.rb
@@ -13,46 +13,34 @@ module Viadeo
 			end
 
 
-     	def simple_query(access_token, path, args)
-     		puts "SIMPLE QUERY"
-        url = "#{DEFAULT_OAUTH_OPTIONS[:api_base]}#{path}?access_token=#{access_token}"
-        args.each {|key, value| url += "&#{key}=#{CGI.escape(value.to_s)}"}
-			  uri = URI.parse(url)
-			  (1..3).each do
-					connection = Net::HTTP.new(uri.host, 443)
-				 	connection.use_ssl = true
-			 	  connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
-				  begin
-				 	  resp = connection.request_get(uri.path + '?' + uri.query)
-					rescue
-						puts "Error: #{$!}"
-						return nil
-					end
-					return Mash.from_json resp.body if resp.code == '200'
-					puts resp.code
-				end
-				return nil
-			end
+		def simple_query(access_token, path, args)
+	        url = "#{DEFAULT_OAUTH_OPTIONS[:api_base]}#{path}?access_token=#{access_token}"
+	        args.each {|key, value| url += "&#{key}=#{CGI.escape(value.to_s)}"}
+			uri = URI.parse(url)
+			(1..3).each do
+				connection = Net::HTTP.new(uri.host, 443)
+				connection.use_ssl = true
+				connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
+				resp = connection.request_get(uri.path + '?' + uri.query)
+				return Mash.from_json resp if resp.code == '200'
+			end			
+			return Mash.from_json resp
+		end
 
      	def simple_post_query(access_token, path, args)
-        url = "#{DEFAULT_OAUTH_OPTIONS[:api_base]}#{path}?access_token=#{access_token}"
-        post_args = ""
-        args.each {|key, value| url += "&#{key}=#{CGI.escape(value.to_s)}"}
-			  uri = URI.parse(url)
-			  (1..3).each do
-					connection = Net::HTTP.new(uri.host, 443)
-				 	connection.use_ssl = true
-			 	  connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
-				  begin
-				 	  resp = connection.request_post(uri.path + '?' + uri.query, post_args)
-					rescue
-						puts "Error: #{$!}"
-						return nil
-					end
-					return Mash.from_json resp.body if resp.code == '200'
-				end
-				return nil
+	        url = "#{DEFAULT_OAUTH_OPTIONS[:api_base]}#{path}?access_token=#{access_token}"
+	        post_args = ""
+	        args.each {|key, value| url += "&#{key}=#{CGI.escape(value.to_s)}"}
+			uri = URI.parse(url)
+			(1..3).each do
+				connection = Net::HTTP.new(uri.host, 443)
+			 	connection.use_ssl = true
+				connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
+				resp = connection.request_post(uri.path + '?' + uri.query, post_args)
+				return Mash.from_json resp if resp.code == '200'
 			end
+			return Mash.from_json resp
+		end
 
         def person_path(options)
           path = "/people/"
